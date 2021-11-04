0dafd4d4fd4270df6a280df95056f2af82e9251c
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-11-04 01:03:48 -0300

    Adição do código base do lab3

---
+++ lab3/src/tiro.cpp
@@ -0,0 +1,11 @@
+#include "../include/tiro.h"
+#include <math.h>
+#define DISTANCIA_MAX 500
+
+void Tiro::DesenhaCirc(GLint radius, GLfloat R, GLfloat G, GLfloat B) {}
+
+void Tiro::DesenhaTiro(GLfloat x, GLfloat y) {}
+
+void Tiro::Move() {}
+
+bool Tiro::Valido() {}
